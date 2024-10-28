17ece08e1fecac6a2ad08487dc909abf56c4ce95
groveeee <asynchronized@yeah.net>
2024-10-28 22:59:06 +0800

    first commit

---
+++ scripts/main.gd
@@ -0,0 +1,52 @@
+extends Node2D
+# ENet是一个网络库 只使用UDP
+# ENetMultiplayerPeer是MultiplayerPeer 的一种实现
+var peer = ENetMultiplayerPeer.new()
+const PLAYER = preload("res://tscns/player.tscn")
+@onready var players: Node = $Players
+
+
+# 创建游戏服务器
+func _on_create_game_server_button_button_down() -> void:
+	# 创建服务器
+	var error = peer.create_server(7788)
+	if error != OK:
+		print("创建服务器失败")
+		return
+	else:
+		print("创建服务器成功")
+	# 绑定服务器
+	# 每个节点都会有一个multiplayer属性 它是对场景树为其配置的MultiplayerAPI实例的引用 multiplayer_peer就是MultiplayerAPI的一个属性 也就是说MultiplayerAPI管理着multiplayer_peer
+	
+	# MultiplayerPeer是管理与一个或多个作为服务器或客户端的远程对等体的连接
+	# 根据对等体本身的不同，该 MultiplayerAPI 
+	# 可能会成为网络服务器（使用 is_server 判断）并将根节点的网络模式设置为控制者，也可能会成为普通的客户端对等体。所有子节点默认会继承其网络模式。
+	# 那么这段代码的含义就是:让当前节点(包括子节点)加入网络 成为网络的控制者(create_server)或普通参与者(create_client)
+	multiplayer.multiplayer_peer = peer
+	# 添加服务端的玩家
+	add_player(multiplayer.get_unique_id())
+	# 作为服务端 监听玩家加入事件
+	multiplayer.peer_connected.connect(_on_peer_connected)
+
+func add_player(id:int) -> void:
+	print("开始添加玩家"+str(id))
+	# 实例化一个玩家场景
+	print("开始实例化场景")
+	var player = PLAYER.instantiate()
+	print("场景实例化完成")
+	player.name = str(id)
+	print("添加玩家到场景树")
+	players.add_child(player)
+	print("添加玩家到场景树完成")
+
+func _on_peer_connected(id: int) -> void:
+	print("玩家"+str(id)+"加入服务器")
+	# 添加玩家
+	add_player(id)
+
+
+# 加入游戏服务器
+func _on_join_game_server_button_button_down() -> void:
+	peer.create_client("127.0.0.1", 7788)
+	multiplayer.multiplayer_peer = peer
+
---
+++ scripts/player.gd
@@ -0,0 +1,83 @@
+extends Sprite2D
+
+# 蓝色小人
+const TILE_0142 = preload("res://assest/kenney_tiny-battle/Tiles/tile_0142.png")
+# 红色小人
+const TILE_0160 = preload("res://assest/kenney_tiny-battle/Tiles/tile_0160.png")
+# 一级子弹
+const BULLET_ONE = preload("res://tscns/bullets/bullet_one.tscn")
+# 移动速度
+const MOVE_SPEED = 100
+
+# 最先调用 实例化的时候
+func _init() -> void:
+	print("_init")
+
+# 其次调用 当节点进入场景树时调用
+func _enter_tree() -> void:
+	print("_enter_tree:设置该节点权限"+name)
+	# 设置该节点的多人权限
+	set_multiplayer_authority(name.to_int())
+
+# 在节点及其所有子节点都已添加到场景树后调用
+func _ready() -> void:
+	print("_ready")
+	# 服务器是红色小人
+	if name == "1":
+		texture = TILE_0160
+	else:
+		texture = TILE_0142
+	print(texture.resource_name)
+	# texture = TILE_0142
+	# 随机设置玩家初始位置
+	position = Vector2(randf_range(0, 800), randf_range(0, 600))
+	print(position)
+	# position = Vector2(300, 300)
+
+func _process(delta: float) -> void:
+	# 如果不是该节点的控制者 不做任何处理
+	# 根据设置的节点的权限id 与 本身的唯一id作对比
+	if not is_multiplayer_authority():
+		return
+	# 实现玩家的移动
+	var input_vector = Vector2()
+	if Input.is_action_pressed("right"):
+		input_vector.x += 1
+	if Input.is_action_pressed("left"):
+		input_vector.x -= 1
+	if Input.is_action_pressed("up"):
+		input_vector.y -= 1
+	if Input.is_action_pressed("down"):
+		input_vector.y += 1
+	
+	if input_vector.length() > 0:
+		input_vector = input_vector.normalized()
+	
+	position += input_vector * MOVE_SPEED * delta
+	# 攻击
+	if Input.is_action_just_pressed("attack"):
+		attack()
+
+# 攻击的方法
+func attack():
+	print("我是:"+str(multiplayer.get_unique_id()))
+	print(name+":进行了攻击")
+	# 必须先获取位置后再同步过去 不然两边的子弹方向不一致
+	var direction = get_direction()
+	async_attack.rpc(direction,position)
+
+# 同步发射出去的子弹
+@rpc("any_peer","call_local")
+func async_attack(direction:Vector2,pos:Vector2):
+	var bullet_one = BULLET_ONE.instantiate()
+	bullet_one.direction = direction
+	bullet_one.position = position
+	get_parent().add_child(bullet_one)
+	
+
+# 获取鼠标所在位置的方向
+func get_direction() -> Vector2:
+	var mouse_pos = get_global_mouse_position()
+	var direction = (mouse_pos - global_position).normalized()
+	return direction
+	
