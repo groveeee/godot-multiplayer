89d7bc9aa791a86f397eb16d6cd8f30b1c2a169e
groveeee <asynchronized@yeah.net>
2024-10-29 21:45:11 +0800

    鼠标移动

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,7 +8,10 @@ const TILE_0160 = preload("res://assest/kenney_tiny-battle/Tiles/tile_0160.png")
 const BULLET_ONE = preload("res://tscns/bullets/bullet_one.tscn")
 # 移动速度
 const MOVE_SPEED = 100
+# 玩家相机
 @onready var camera_2d: Camera2D = $Camera2D
+# 需要移动到的目标位置
+var target_pos: Vector2 = Vector2.ZERO
 
 # 最先调用 实例化的时候
 func _init() -> void:
@@ -44,29 +47,46 @@ func _ready() -> void:
 	print(position)
 	# position = Vector2(300, 300)
 
+
+# func _input(event: InputEvent) -> void:
+# 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT:
+# 		print("点击了鼠标右键")
+		
 func _process(delta: float) -> void:
 	# 如果不是该节点的控制者 不做任何处理
 	# 根据设置的节点的权限id 与 本身的唯一id作对比
 	if not is_multiplayer_authority():
 		return
-	# 实现玩家的移动
-	var input_vector = Vector2()
-	if Input.is_action_pressed("right"):
-		input_vector.x += 1
-	if Input.is_action_pressed("left"):
-		input_vector.x -= 1
-	if Input.is_action_pressed("up"):
-		input_vector.y -= 1
-	if Input.is_action_pressed("down"):
-		input_vector.y += 1
+	# 实现玩家的移动(WASD)
+	# var input_vector = Vector2()
+	# if Input.is_action_pressed("right"):
+	# 	input_vector.x += 1
+	# if Input.is_action_pressed("left"):
+	# 	input_vector.x -= 1
+	# if Input.is_action_pressed("up"):
+	# 	input_vector.y -= 1
+	# if Input.is_action_pressed("down"):
+	# 	input_vector.y += 1
 	
-	if input_vector.length() > 0:
-		input_vector = input_vector.normalized()
+	# if input_vector.length() > 0:
+	# 	input_vector = input_vector.normalized()
 	
-	position += input_vector * MOVE_SPEED * delta
+	# position += input_vector * MOVE_SPEED * delta
 	# 攻击
 	if Input.is_action_just_pressed("attack"):
 		attack()
+	# 玩家移动(鼠标右键点击)
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		target_pos = get_global_mouse_position()
+		print("点击了鼠标右键")
+		print(target_pos)
+	# 如果有目标位置就移动到目标位置
+	if target_pos != Vector2.ZERO:
+		move_towards_target(delta)
+	# 到达目的地
+	if position.distance_to(target_pos)<10:
+		target_pos = Vector2.ZERO
+
 
 # 攻击的方法
 func attack():
@@ -74,11 +94,11 @@ func attack():
 	print(name+":进行了攻击")
 	# 必须先获取位置后再同步过去 不然两边的子弹方向不一致
 	var direction = get_direction()
-	async_attack.rpc(direction,position)
+	async_attack.rpc(direction)
 
 # 同步发射出去的子弹
 @rpc("any_peer","call_local")
-func async_attack(direction:Vector2,pos:Vector2):
+func async_attack(direction:Vector2):
 	var bullet_one = BULLET_ONE.instantiate()
 	bullet_one.direction = direction
 	bullet_one.position = position
@@ -87,7 +107,13 @@ func async_attack(direction:Vector2,pos:Vector2):
 
 # 获取鼠标所在位置的方向
 func get_direction() -> Vector2:
-	var mouse_pos = get_global_mouse_position()
+	var mouse_pos = get_local_mouse_position()
 	var direction = (mouse_pos - global_position).normalized()
 	return direction
+
+# 朝着鼠标双击的位置移动
+# 移动到目标位置
+func move_towards_target(delta: float):
+	var direction = (target_pos - global_position).normalized()
+	position += direction * MOVE_SPEED * delta
 	
